02a0916a96d4d0a2361f9f9e4f1f4a6ba5c95780
aguang5241 <gliu4@wpi.edu>
2024-08-20 10:38:46 -0400

    modified:   .DS_Store modified:   README.md modified:   make.py

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Frame_Maker
 Create a photo frame and display the information extracted from the photo's metadata
 
+⭐️ Star the repository if you find it useful :) ⭐️
+
+## Usage
+1. Copy the photos you want to frame into the `input` folder
+2. Run the `frame_maker.py` script
+```
+python frame_maker.py
+```
+3. The framed photos will be saved in the `output` folder
+
 ## Before and After
 <!-- ![Before](./DSC_0043.jpg)
 ![After](./DSC_0043_new.jpg) -->
--- make.py
+++ make.py
@@ -7,6 +7,7 @@ def main(path):
     exif = { ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS }
     # print(img._getexif().items())
     # exit()
+    # Get content created time
     img_format = img.format
     img_mode = img.mode
     img_size = os.path.getsize(path) / (1024 * 1024)
@@ -94,22 +95,22 @@ def main(path):
     img_new_iso = exif_new['ISOSpeedRatings']
     img_new_focal_length = f'{int(exif_new["FocalLength"])}'
 
-    # # Display original and new image information
-    # print('\n\t\t\t Before \t\t After')
-    # print(f'Image:\t\t\t {path}\t\t {path_new}')
-    # print(f'Format:\t\t\t {img_format}\t\t\t {img_new_format}')
-    # print(f'Mode:\t\t\t {img_mode}\t\t\t {img_new_mode}')
-    # print(f'Size:\t\t\t {img_size:.2f}MB\t\t\t {img_new_size:.2f}MB')
-    # print(f'Width:\t\t\t {img_width}\t\t\t {img_new_width}')
-    # print(f'Height:\t\t\t {img_height}\t\t\t {img_new_height}')
-    # print(f'Resolution:\t\t {img_resolution}\t\t {img_new_resolution}')
-    # print(f'Make:\t\t\t {img_make}\t\t\t {img_new_make}')
-    # print(f'Model:\t\t\t {img_model}\t\t {img_new_model}')
-    # print(f'Date:\t\t\t {img_date}\t {img_new_date}')
-    # print(f'Shutter:\t\t {img_shutter}\t\t\t {img_new_shutter}')
-    # print(f'Aperture:\t\t {img_aperture}\t\t\t {img_new_aperture}')
-    # print(f'ISO:\t\t\t {img_iso}\t\t\t {img_new_iso}')
-    # print(f'FocalLength:\t\t {img_focal_length}\t\t\t {img_new_focal_length}')
+    # Display original and new image information
+    print('\n\t\t\t Before \t\t After')
+    print(f'Image:\t\t\t {path}\t\t {path_new}')
+    print(f'Format:\t\t\t {img_format}\t\t\t {img_new_format}')
+    print(f'Mode:\t\t\t {img_mode}\t\t\t {img_new_mode}')
+    print(f'Size:\t\t\t {img_size:.2f}MB\t\t\t {img_new_size:.2f}MB')
+    print(f'Width:\t\t\t {img_width}\t\t\t {img_new_width}')
+    print(f'Height:\t\t\t {img_height}\t\t\t {img_new_height}')
+    print(f'Resolution:\t\t {img_resolution}\t\t {img_new_resolution}')
+    print(f'Make:\t\t\t {img_make}\t\t\t {img_new_make}')
+    print(f'Model:\t\t\t {img_model}\t\t {img_new_model}')
+    print(f'Date:\t\t\t {img_date}\t {img_new_date}')
+    print(f'Shutter:\t\t {img_shutter}\t\t\t {img_new_shutter}')
+    print(f'Aperture:\t\t {img_aperture}\t\t\t {img_new_aperture}')
+    print(f'ISO:\t\t\t {img_iso}\t\t\t {img_new_iso}')
+    print(f'FocalLength:\t\t {img_focal_length}\t\t\t {img_new_focal_length}')
 
 if __name__ == '__main__':
     # Get all the images in the input folder
@@ -119,3 +120,4 @@ if __name__ == '__main__':
     for img_path in img_paths:
         main(f'{input_folder}/{img_path}')
         print(f'{img_path} is processed successfully.')
+        exit()
